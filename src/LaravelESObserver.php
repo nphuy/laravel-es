fbec8cec36897fb5b14dc14d87946fe0daf70b7d
Huy Nguyen <huynp.dev@gmail.com>
2022-04-05 09:05:48 +0700

    update

--- src/LaravelESObserver.php
+++ src/LaravelESObserver.php
@@ -1,39 +1,42 @@
 <?php
 
 namespace HNP\LaravelES;
+
 use Illuminate\Database\Eloquent\Model;
 use HNP\LaravelES\Jobs\AddES;
 use HNP\LaravelES\Jobs\UpdateES;
 use HNP\LaravelES\Jobs\RemoveES;
 
-class LaravelESObserver{
+class LaravelESObserver
+{
 
     public function created(Model $model)
     {
-        if(config('hnp_es.queue', false)){
-            AddES::dispatch($model);
-        }else{
-            $model->addToIndex();
+        if ($model->allowIndex()) {
+            if (config('hnp_es.queue', false)) {
+                AddES::dispatch($model);
+            } else {
+                $model->addToIndex();
+            }
         }
-        
     }
     public function updated(Model $model)
     {
-        if(config('hnp_es.queue', false)){
-            UpdateES::dispatch($model);
-        }else{
-            $model->addToIndex();
+        if ($model->allowIndex()) {
+            if (config('hnp_es.queue', false)) {
+                UpdateES::dispatch($model);
+            } else {
+                $model->addToIndex();
+            }
         }
-        
     }
     public function deleting(Model $model)
     {
-        try{
+        try {
             $model->removeFromIndex();
-        }catch(\Exception $e){
-
+        } catch (\Exception $e) {
         }
-        
+
         // RemoveES::dispatch($model);
     }
 }
